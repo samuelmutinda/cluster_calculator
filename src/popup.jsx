ea408b6dffc6f041d30d4116ead535596c9fd7c1
samuel mutinda <mutindasamuel01@gmail.com>
2024-02-11 12:25:45 +0300

    added loading spinner

--- src/popup.jsx
+++ src/popup.jsx
@@ -14,8 +14,10 @@ export function Popup({ trigger, onConfirmPayment, onClose }) {
                     <p className='popupdescription'>
                         You will receive an M-Pesa popup on your phone. Enter your M-Pesa pin to approve the transaction.
                         When you have received the M-Pesa 
-                        transaction message, click Confirm Payment below
+                        transaction message, click Confirm Payment
                     </p>
+                    <p className='popupdescription'>Awaiting Payment..</p>
+                    <div className='loader'></div>
                     <p className='popupdescription2'>
                         Report any payment issues to us. Our technical team 
                         will sort them our promptly.
